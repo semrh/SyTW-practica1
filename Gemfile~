fb54c43e70db051dd5b880b9b56709f98b5e7b94
RAMOS HERRERA <alu4289@cc382.(none)>
2014-09-25 16:03:09 +0100

    Creado configure.rb

--- Gemfile~
+++ Gemfile~
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
 
-gem 'faraday', '0.8.8'
+#gem 'faraday', '0.8.8'
 gem 'twitter'
---
+++ configure.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key       = 'wUOvnI433P3QXWbloU2seg'
+  config.consumer_secret    = 'Kk8Azwl6ksARgWMTdncCpAgGsSzRM2O52vAVJJHPFvM'
+  config.oauth_token        = "385906382-AQ6Ybj8wZZeHBL4cnMhXTtI7K2byBJs9LcXxDE1o"
+  config.oauth_token_secret = "Vx9OjFi04910PLRLWou2oKDYQ1uJUF7LKOjGnI1Ufw"
+end
