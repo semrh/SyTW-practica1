7666bdd79373f425a9316dbecd47027e7091a8de
Sem Ramos <alu0100603194@ull.edu.es>
2014-09-26 07:58:05 +0000

    Cambiando configure.rb

--- configure.rb
+++ configure.rb
@@ -1,6 +1,6 @@
 Twitter.configure do |config|
-  config.consumer_key       = 'wUOvnI433P3QXWbloU2seg'
-  config.consumer_secret    = 'Kk8Azwl6ksARgWMTdncCpAgGsSzRM2O52vAVJJHPFvM'
-  config.oauth_token        = "385906382-AQ6Ybj8wZZeHBL4cnMhXTtI7K2byBJs9LcXxDE1o"
-  config.oauth_token_secret = "Vx9OjFi04910PLRLWou2oKDYQ1uJUF7LKOjGnI1Ufw"
+  config.consumer_key       = 'z8Qtk8oBgM2ykx8igIQa5Dbjq'
+  config.consumer_secret    = 'Ma0mfqGh1iF17vEe6P9lArtwo8Ik8MpZnO5gCjCyYSyLtiN7Ve'
+  config.oauth_token        = "85652810-2hBxsypy8WF6Cadun46cunplIpAhGksndzGRVK1gU"
+  config.oauth_token_secret = "CKldAFFAQc0v7ssVS4RTd7gbcV3FcwHJsZFePCWwi1duJ"
 end
